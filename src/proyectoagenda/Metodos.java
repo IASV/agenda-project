2436693c2a3a464eabe8cdcc0bde7b267e911c35
IASV <iasuarez@unicesar.edu.co>
2020-05-03 12:14:33 -0500

    Versión 0.0

---
+++ src/proyectoagenda/Metodos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoagenda;
+
+/**
+ *
+ * @author Iván Suarez, Kevin García
+ */
+public class Metodos {
+    
+    //Método listarItemAgenda()
+    
+    //Método itemAgenda()
+    
+    //Método buscarItemAgenda()
+    
+    //Método borrarItemAgenda()
+    
+}
---
+++ src/proyectoagenda/ProyectoAgenda.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoagenda;
+import java.util.Scanner;
+/**
+ *
+ * @author Iván Suarez, Kevin García
+ */
+public class ProyectoAgenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        String[][] ajenda = new String[20][20]; 
+        int option = 1;
+        
+        // Menú
+        do{
+            System.out.print("*************** AGENDA ***************"
+                    + "\n\t\t1. Mostrar"
+                    + "\n\t\t2. Añadir"
+                    + "\n\t\t3. Buscar"
+                    + "\n\t\t4. Borrar"
+                    + "\n\t\t0. Terminar"
+                    + "\n**************************************"
+                    + "\nElige una opción: ");
+            option = teclado.nextInt();
+        }while(option != 0);
+    }
+}
